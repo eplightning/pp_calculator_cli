1cb6e4fd67fb6f1c214da3c8d50a2a280c9abccb
eplightning <eplightning@outlook.com>
2014-12-06 14:05:55 +0100

    Poprawione wykrywanie cichego trybu

--- core/main.cpp
+++ core/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     bool verbose = true;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-q") == 0 || strcmp(argv[i], "--quiet"))
+        if (strcmp(argv[i], "-q") == 0 || strcmp(argv[i], "--quiet") == 0)
             verbose = false;
     }
 
